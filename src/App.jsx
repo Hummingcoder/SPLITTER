533c1b0b42863f3d197d095ff8a8945ed21ff85e
Vishal D. <dwivedivishal0702@gmail.com>
2024-08-19 09:50:35 +0530

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ const App = () => {
       <main className="font-SpaceMono w-full min-h-screen bg-lightgrayishcyan grid place-content-center">
         <img
           className="w-[70px] md:mt-[4vh] mx-auto mt-[8vh]"
-          src="images/logo.svg"
+          src="./images/logo.svg"
           alt="logo-image"
         />
         <section className="bg-white mt-[6vh] w-[100%]  mx-auto rounded-2xl max-w-[800px] shadow-lg grid grid-cols-1 md:grid-cols-2 gap-8 p-6 mb-12">
